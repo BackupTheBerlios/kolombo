fb8a3abba1c4ce3c148b367f9e14e4a1ea9c08f2
agarfu <agarfu>
2006-08-24 08:43:44 +0000

    last resources

---
+++ kolombo/python/resources/lang/kolombo_es.ts
@@ -0,0 +1,346 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Error importing data</source>
+        <translation type="obsolete">Error importanto datos</translation>
+    </message>
+    <message>
+        <source>Successfuly created database</source>
+        <translation type="obsolete">Base de datos creada correctamente</translation>
+    </message>
+    <message>
+        <source>New database created.
+The database has been populated with some data</source>
+        <translation type="obsolete">Nueva base de datos creada.
+Se han añadido datos iniciales a la nueva base de datos</translation>
+    </message>
+    <message>
+        <source>Cannot open database</source>
+        <translation type="obsolete">No se puede abrir la base de datos</translation>
+    </message>
+    <message>
+        <source>Unable to establish a database connection.
+This application needs SQLite support. Please read the Qt SQL driver documentation for information how to build it.
+
+Click Cancel to exit.</source>
+        <translation type="obsolete">No se ha podido establecer la conexión con la base de datos.
+Esta aplicación necesita soporte de SQLite. Por favor, lea la documentación del driver de SQL de  Qt para saber como construirlo.
+
+Pulse cancelar para salir.</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Filter:&lt;/b&gt;</source>
+        <translation type="obsolete">&lt;b&gt;Filtro:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>Nacionality</source>
+        <translation type="obsolete">Nacionalidad</translation>
+    </message>
+    <message>
+        <source>Eye</source>
+        <translation type="obsolete">Ojo</translation>
+    </message>
+    <message>
+        <source>Plumage</source>
+        <translation type="obsolete">Plumaje</translation>
+    </message>
+    <message>
+        <source>M</source>
+        <translation type="obsolete">M</translation>
+    </message>
+    <message>
+        <source>F</source>
+        <translation type="obsolete">V</translation>
+    </message>
+    <message>
+        <source>L</source>
+        <translation type="obsolete">D</translation>
+    </message>
+    <message>
+        <source>R</source>
+        <translation type="obsolete">R</translation>
+    </message>
+    <message>
+        <source>Error: ivalid index</source>
+        <translation type="obsolete">Error: índice inválido</translation>
+    </message>
+    <message>
+        <source>Change sex</source>
+        <translation type="obsolete">Cambiar sexo</translation>
+    </message>
+    <message>
+        <source>Flyer</source>
+        <translation type="obsolete">Viajera</translation>
+    </message>
+    <message>
+        <source>Reproducer</source>
+        <translation type="obsolete">Reproductora</translation>
+    </message>
+    <message>
+        <source>Lost</source>
+        <translation type="obsolete">Desaparecida</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="obsolete">Eliminar</translation>
+    </message>
+    <message>
+        <source>View Reproduction</source>
+        <translation type="obsolete">Ver reproducción</translation>
+    </message>
+    <message>
+        <source>View Flyers</source>
+        <translation type="obsolete">Ver viajeras</translation>
+    </message>
+    <message>
+        <source>View Lost</source>
+        <translation type="obsolete">Ver desaparecidas</translation>
+    </message>
+    <message>
+        <source>View All</source>
+        <translation type="obsolete">Ver todas</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation type="obsolete">Imprimir</translation>
+    </message>
+    <message>
+        <source>Piggeon menu</source>
+        <translation type="obsolete">Menú de paloma</translation>
+    </message>
+    <message>
+        <source>Change state</source>
+        <translation type="obsolete">Cambiar estado</translation>
+    </message>
+    <message>
+        <source>Print Document</source>
+        <translation type="obsolete">Imprimir documento</translation>
+    </message>
+</context>
+<context>
+    <name>QtCore</name>
+    <message>
+        <source>Nacionality</source>
+        <translation>Nacionalidad</translation>
+    </message>
+    <message>
+        <source>Eye</source>
+        <translation>Ojo</translation>
+    </message>
+    <message>
+        <source>Plumage</source>
+        <translation>Plumaje</translation>
+    </message>
+    <message>
+        <source>Year</source>
+        <translation>Año</translation>
+    </message>
+    <message>
+        <source>Identification</source>
+        <translation>Anilla</translation>
+    </message>
+    <message>
+        <source>Sex</source>
+        <translation>Sexo</translation>
+    </message>
+    <message>
+        <source>State</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <source>Mother</source>
+        <translation>Madre</translation>
+    </message>
+    <message>
+        <source>Father</source>
+        <translation>Padre</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+</context>
+<context>
+    <name>QtGui.qApp</name>
+    <message>
+        <source>Error importing data</source>
+        <translation>Error importanto datos</translation>
+    </message>
+    <message>
+        <source>Successfuly created database</source>
+        <translation>Base de datos creada correctamente</translation>
+    </message>
+    <message>
+        <source>New database created.
+The database has been populated with some data</source>
+        <translation>Nueva base de datos creada.
+Se han añadido datos iniciales a la nueva base de datos</translation>
+    </message>
+    <message>
+        <source>Cannot open database</source>
+        <translation>No se puede abrir la base de datos</translation>
+    </message>
+    <message>
+        <source>Unable to establish a database connection.
+This application needs SQLite support. Please read the Qt SQL driver documentation for information how to build it.
+
+Click Cancel to exit.</source>
+        <translation>No se ha podido establecer la conexión con la base de datos.
+Esta aplicación necesita soporte de SQLite. Por favor, lea la documentación del driver de SQL de  Qt para saber como construirlo.
+
+Pulse cancelar para salir.</translation>
+    </message>
+</context>
+<context>
+    <name>dovecoteView</name>
+    <message>
+        <source>&lt;b&gt;Filter:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Filtro:&lt;/b&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>firstRunDialog</name>
+    <message>
+        <source>Kolombo initial settings</source>
+        <translation type="obsolete">Configuración inicial de Kolombo</translation>
+    </message>
+    <message>
+        <source>Database path:</source>
+        <translation type="obsolete">Ruta de la base de datos:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Welcome to kolombo</source>
+        <translation>Bienvenido a kolombo</translation>
+    </message>
+    <message>
+        <source>&lt;p align=&quot;center&quot;&gt;Welcome to &lt;b&gt;kolombo&lt;/b&gt;.&lt;/p&gt;
+
+&lt;p&gt;This is the firs time that
+you use this application.&lt;/p&gt;
+
+&lt;p&gt;Please tell me where do
+you want to store your data.&lt;/p&gt;</source>
+        <translation type="unfinished">&lt;p align=&quot;center&quot;&gt;Bienvenido a &lt;b&gt;kolombo&lt;/b&gt;.&lt;/p&gt;
+
+&lt;p&gt;Esta es la primera vez que
+usted utiliza esta aplicación.&lt;/p&gt;
+
+&lt;p&gt;Dígame dónde desa almacenar
+los datos de su palomar.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Database path</source>
+        <translation>Rua a la base de datos</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+<context>
+    <name>kolomboCentralWidget</name>
+    <message>
+        <source>Lists</source>
+        <translation>Listado</translation>
+    </message>
+    <message>
+        <source>New Piggeon</source>
+        <translation>Nueva paloma</translation>
+    </message>
+    <message>
+        <source>Pairs</source>
+        <translation>Parejas</translation>
+    </message>
+</context>
+<context>
+    <name>palomarSqlModel</name>
+    <message>
+        <source>M</source>
+        <translation>M</translation>
+    </message>
+    <message>
+        <source>F</source>
+        <translation>V</translation>
+    </message>
+    <message>
+        <source>L</source>
+        <translation>D</translation>
+    </message>
+    <message>
+        <source>R</source>
+        <translation>R</translation>
+    </message>
+    <message>
+        <source>Error: ivalid index</source>
+        <translation>Error: índice inválido</translation>
+    </message>
+    <message>
+        <source>No information</source>
+        <translation>Sin información</translation>
+    </message>
+</context>
+<context>
+    <name>palomarTableView</name>
+    <message>
+        <source>Change sex</source>
+        <translation>Cambiar sexo</translation>
+    </message>
+    <message>
+        <source>Flyer</source>
+        <translation>Viajera</translation>
+    </message>
+    <message>
+        <source>Reproducer</source>
+        <translation>Reproductora</translation>
+    </message>
+    <message>
+        <source>Lost</source>
+        <translation>Desaparecida</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <source>View Reproduction</source>
+        <translation>Ver reproducción</translation>
+    </message>
+    <message>
+        <source>View Flyers</source>
+        <translation>Ver viajeras</translation>
+    </message>
+    <message>
+        <source>View Lost</source>
+        <translation>Ver desaparecidas</translation>
+    </message>
+    <message>
+        <source>View All</source>
+        <translation>Ver todas</translation>
+    </message>
+    <message>
+        <source>Print</source>
+        <translation>Imprimir</translation>
+    </message>
+    <message>
+        <source>Piggeon menu</source>
+        <translation>Menú de paloma</translation>
+    </message>
+    <message>
+        <source>Change state</source>
+        <translation>Cambiar estado</translation>
+    </message>
+    <message>
+        <source>Print Document</source>
+        <translation>Imprimir documento</translation>
+    </message>
+</context>
+</TS>
